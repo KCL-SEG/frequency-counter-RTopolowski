7f47c2feb3f58af2693023d60deabe7514766cf3
Robert Topolowski <robert.topolowski@kcl.ac.uk>
2022-10-02 21:12:20 +0100

    Implement frequency counting functionality

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for i in range(0, len(items)):
+        item = str(items[i])
+
+        if item in frequencies:
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
